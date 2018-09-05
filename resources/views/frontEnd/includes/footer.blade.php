fe31486f6f299590540504b2c5552349ee0f07b7
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-09-06 02:00:29 +0700

    qwqwe

--- resources/views/frontEnd/includes/footer.blade.php
+++ resources/views/frontEnd/includes/footer.blade.php
@@ -16,19 +16,7 @@
                         <img src="/uploads/settings/{{ $WebsiteSettings->style_footer_bg }}">
                     </div>
                     <div class="main-footer-footer-content">
-                        <p class="license">
-                            © Cổng thông tin điện tử tỉnh Hà Tĩnh
-                        </p>
-                        <p class="main-footer-info-text">
-                            Giấy phép số 27/GP-STTTT cấp ngày 25/5/2018 của Sở Thông tin & Truyền thông Hà Tĩnh
-                            <br>
-                            <strong>Trưởng ban Ban biên tập: Ông Phan Tấn Linh, Giám Đốc Sở Thông tin - Truyền Thông</strong>
-                            <br>
-                            <em>Địa chỉ: 66 Phan Đình Phùng, TP. Hà Tĩnh. Email: <a style="color:#FFF" href="mailto:portalhatinh@hatinh.gov.vn">portalhatinh@hatinh.gov.vn</a></em>
-                            <br>
-                            <em>Điện thoại: (+84.239) 3686586, Fax:(+84.239) 3850288</em>
-                            <br> Ghi rõ nguồn Cổng Thông tin điện tử tỉnh Hà Tĩnh hoặc <a style="color:#FFF" href="http://www.hatinh.gov.vn/">www.hatinh.gov.vn</a> khi bạn phát hành lại thông tin từ Website này.
-                        </p>
+                        {!! $WebsiteSettings->site_footer !!}
                     </div>
                 </div>
             </div>
