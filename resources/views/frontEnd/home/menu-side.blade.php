c729630c39eb4c8aca41916d332352a9642bfa15
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-09-06 02:51:43 +0700

    qqqqqqqe

--- resources/views/frontEnd/home/menu-side.blade.php
+++ resources/views/frontEnd/home/menu-side.blade.php
@@ -7,28 +7,3 @@
 
 @extends('frontEnd.includes.menu-side')
 
-@section('section-menu')
-    
-    <div class="block4" style="border-radius: 5px">
-
-        <div id="block-header-bd" class="block-header" style="margin-bottom: 0" data-toggle="collapse" href="#ban-do-dia-gioi">
-
-            <h4>
-                <img src="/images/background/lotus.ico" alt="" width="26px"> {!! trans('frontLang.map') !!}
-
-                <i id="menu-bd" class="fa fa-chevron-down" style="position: absolute; top: 5px; right:12px;left: auto"></i>
-            </h4>
-
-        </div>
-
-        <div id="ban-do-dia-gioi" class="panel-collapse collapse in">
-            <a href="http://gis.chinhphu.vn/?r=d8urwW36Eim5fYUZebkjQ" target="_blank">
-
-                <img src="/images/ban-do.jpg" alt="Bản đồ Hà Tĩnh" title="Bản đồ Hà Tĩnh" width="100%">
-            </a>
-        </div>
-
-    </div>
-
-    
-@endsection
