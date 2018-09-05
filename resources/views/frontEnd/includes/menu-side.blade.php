dedcbe2c42a94748037417dd36d07b120dc66f07
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-09-06 02:43:02 +0700

    sdsad

--- resources/views/frontEnd/includes/menu-side.blade.php
+++ resources/views/frontEnd/includes/menu-side.blade.php
@@ -25,49 +25,7 @@
 
     
 
-@if (!empty($RightMenuLinks))
-<div class="right_1">
-        
-    @foreach($RightMenuLinks as $RightMenuLink)
 
-        <?php
-            if ($RightMenuLink->webmasterSection[$slug_var] != "" && Helper::GeneralWebmasterSettings("links_status")) {
-                if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                    $mmnnuu_link = url(trans('backLang.code')."/" .$RightMenuLink->webmasterSection[$slug_var]);
-                }else{
-                    $mmnnuu_link = url($RightMenuLink->webmasterSection[$slug_var]);
-                }
-            }else{
-                if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                    $mmnnuu_link =url(trans('backLang.code')."/" .$RightMenuLink->webmasterSection['name']);
-                }else{
-                    $mmnnuu_link =url($RightMenuLink->webmasterSection['name']);
-                }
-            }
-        ?>
-
-        <div class="right-item">
-    
-            <a href="{{ (trim($RightMenuLink->link) !="") ? $RightMenuLink->link:$mmnnuu_link }}" class="icon" title="">
-
-                @if (!empty($RightMenuLink->icon))
-                    
-                    <i class="fa {{$RightMenuLink->icon}} fa-2x" aria-hidden="true"></i>
-
-                @else
-    
-                    <i class="fa fa-folder-open-o fa-2x" aria-hidden="true"></i>
-
-                @endif
-    
-                <span class="nav-text">{{ $RightMenuLink->$title_var }} </span>
-            </a>
-        </div>
-    
-    @endforeach
-
-</div>
-@endif
 
 
 <div class="block4" style="border-radius: 5px">
@@ -196,6 +154,50 @@
         </div>
     </div>
 </div>
+
+@if (!empty($RightMenuLinks))
+<div class="right_1">
+        
+    @foreach($RightMenuLinks as $RightMenuLink)
+
+        <?php
+            if ($RightMenuLink->webmasterSection[$slug_var] != "" && Helper::GeneralWebmasterSettings("links_status")) {
+                if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
+                    $mmnnuu_link = url(trans('backLang.code')."/" .$RightMenuLink->webmasterSection[$slug_var]);
+                }else{
+                    $mmnnuu_link = url($RightMenuLink->webmasterSection[$slug_var]);
+                }
+            }else{
+                if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
+                    $mmnnuu_link =url(trans('backLang.code')."/" .$RightMenuLink->webmasterSection['name']);
+                }else{
+                    $mmnnuu_link =url($RightMenuLink->webmasterSection['name']);
+                }
+            }
+        ?>
+
+        <div class="right-item">
+    
+            <a href="{{ (trim($RightMenuLink->link) !="") ? $RightMenuLink->link:$mmnnuu_link }}" class="icon" title="">
+
+                @if (!empty($RightMenuLink->icon))
+                    
+                    <i class="fa {{$RightMenuLink->icon}} fa-2x" aria-hidden="true"></i>
+
+                @else
+    
+                    <i class="fa fa-folder-open-o fa-2x" aria-hidden="true"></i>
+
+                @endif
+    
+                <span class="nav-text">{{ $RightMenuLink->$title_var }} </span>
+            </a>
+        </div>
+    
+    @endforeach
+
+</div>
+@endif
 
 
 {{--  <div class="clearfix"></div>
