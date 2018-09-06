1a4047b42e96206053aa17b1e0f3bbe2239de768
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-09-06 23:20:53 +0700

    asdsa

--- resources/views/frontEnd/calendar.blade.php
+++ resources/views/frontEnd/calendar.blade.php
@@ -7,7 +7,7 @@
     <section id="content">
         <div class="block3">
 
-            <div class="portlet-header" style="margin-bottom:0; height: 800px">
+            <div class="portlet-header" style="margin-bottom:0">
                 
                     <ul class="breadcrumb">
                         <li><a href="{{ route('Home') }}"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i>
@@ -19,7 +19,7 @@
 
             
             
-            <div class="col-md-12" style="padding:0">
+            <div class="col-md-12" style="padding:0; height: 800px">
 
                 <iframe src="lich-cong-tac-iframe" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
             
