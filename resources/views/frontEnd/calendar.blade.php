fc760315d1a85545ccbde2517bbd2571f832890d
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-09-06 22:20:23 +0700

    sadsad

--- resources/views/frontEnd/calendar.blade.php
+++ resources/views/frontEnd/calendar.blade.php
@@ -19,9 +19,9 @@
 
             
             
-            <div class="col-md-12">
+            <div class="col-md-12" style="margin:0px;padding:0px;overflow:hidden">
 
-                <iframe src="http://halinh.dungthinh.com/admin/calendar" frameborder="0" style="overflow:hidden;height:150%;width:150%" height="150%" width="150%"></iframe>
+                <iframe src="http://halinh.dungthinh.com/admin/calendar" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
 
                     
             
